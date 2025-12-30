9b1d149503e80df4b57723d1e55a3f219638bc71
SergioAngel-1 <sergioja2003@gmail.com>
2025-12-29 20:13:25 -0500

    feat: add preconnect and dns-prefetch hints for Google Fonts and Tag Manager to improve resource loading performance

--- main/app/head.tsx
+++ main/app/head.tsx
@@ -3,8 +3,18 @@ import React from 'react';
 export default function Head() {
   return (
     <>
+      {/* Preconnect to external CDNs */}
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+      <link rel="preconnect" href="https://www.googletagmanager.com" />
       <link rel="preconnect" href="https://api.sergioja.com" />
+      
+      {/* DNS prefetch fallback */}
+      <link rel="dns-prefetch" href="https://fonts.googleapis.com" />
+      <link rel="dns-prefetch" href="https://fonts.gstatic.com" />
+      <link rel="dns-prefetch" href="https://www.googletagmanager.com" />
       <link rel="dns-prefetch" href="https://api.sergioja.com" />
+      
       <meta name="mobile-web-app-capable" content="yes" />
       <meta name="apple-mobile-web-app-capable" content="yes" />
       <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
